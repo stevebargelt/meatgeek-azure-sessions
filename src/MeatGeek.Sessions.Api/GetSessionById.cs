5067f9e6809a913c5743bbe82770dd146a75d5da
Steve Bargelt <steve@bargelt.com>
2021-05-17 15:53:30 -0700

    Requireing SmokerID

--- src/MeatGeek.Sessions.Api/GetSessionById.cs
+++ src/MeatGeek.Sessions.Api/GetSessionById.cs
@@ -21,17 +21,18 @@ namespace MeatGeek.Sessions
     {
         [FunctionName("GetSessionById")]
         [OpenApiOperation(operationId: "GetSessionById", tags: new[] { "session" }, Summary = "Find Session by ID", Description = "Returns a single session. Sessions are cooking / BBQ Sessions or cooks.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiParameter(name: "snokerid", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "The ID of the Smoker the session belings to", Description = "The ID of the Smoker the session belings to", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiParameter(name: "id", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "ID of the Session to return", Description = "The ID of the session to return", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Session), Summary = "successful operation", Description = "successful response")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "Session not found", Description = "Session not found")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sessions/{id}")] HttpRequest req, 
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sessions/{smokerid}/{id}")] HttpRequest req, 
                 [CosmosDB(
                 databaseName: "Sessions",
                 collectionName: "sessions",
                 ConnectionStringSetting = "CosmosDBConnection",
-                Id = "{id}")] Object session,
+                Id = "{id}", PartitionKey = "{smokerid}")] Object session,
             ILogger log)
         {
             log.LogInformation("GetSessionById triggered");
